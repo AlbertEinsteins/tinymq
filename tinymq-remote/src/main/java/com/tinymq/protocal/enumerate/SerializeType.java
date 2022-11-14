98ac2afc16734225ef454431abb9e3197ec422b9
Jin Feihu <2243883304@qq.com>
2022-11-14 23:38:54 +0800

    set package type

---
+++ tinymq-remote/src/main/java/com/tinymq/protocal/enumerate/SerializeType.java
@@ -0,0 +1,32 @@
+package com.tinymq.protocal.enumerate;
+
+/**
+ * 通信过程的序列化类型
+ */
+public enum SerializeType {
+    // JSO序列化
+    JSON_SERIALIZE((byte)1),
+    // Hessian序列化
+    HESSIAN_SERIALIZE((byte)2),
+    // JDK 提供的序列化
+    JDK_SERIALIZE((byte)3),
+    // Google Protobuf 序列化
+    PROTOBUF_SERIALIZE((byte)3);
+
+
+    public byte code;
+    SerializeType(byte code) {
+        this.code = code;
+    }
+
+
+    public static SerializeType fromByteCode(byte code) {
+        switch (code) {
+            default:
+            case (1 << 2): return SerializeType.JDK_SERIALIZE;
+            case (1): return JSON_SERIALIZE;
+            case (1 << 1): return HESSIAN_SERIALIZE;
+            case (1 << 3): return PROTOBUF_SERIALIZE;
+        }
+    }
+}
